fd93a439d6bc122113fc07e56cd846e869eb5502
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-02-10 22:46:51 +0200

    added content

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello!");
+function create_message() {
+    return {
+        hogwarts_houses: ['Slytherin', 'Gryffindor', 'Ravenclaw', 'Hufflepuff'],
+        pets: ['cat', 'owl', 'phoenix', 'hippogriffs', 'dragon', 'toad'],
+        subjects: ['Herbology', 'Defense Against The Dark Arts', 'Charms', 'Divination', 'Transfiguration', 'Astronomy', 'Potions', 'Flying Lessons'],
+        quotes: ['“Do not pity the dead, Harry. Pity the living, and, above all those who live without love.”– Albus Dumbledore, Harry Potter and the Deathly Hallows, Chapter 35', '“It is impossible to manufacture or imitate love”.– Horace Slughorn, Harry Potter and the Half-Blood Prince, Chapter 9', '“It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.”– Albus Dumbledore, Harry Potter and the Sorcerer’s Stone, Chapter 17', '“Books! And cleverness! There are more important things — friendship and bravery.– Hermione Granger, Harry Potter and the Sorcerer’s Stone, Chapter 16', '“It does not do to dwell on dreams and forget to live.” – Albus Dumbledore, Harry Potter and the Sorcerer’s Stone, Chapter 12', '“Every human life is worth the same, and worth saving.” — Kingsley Shacklebolt, Harry Potter and the Deathly Hallows, Chapter 22', '“‘The thing about growing up with Fred and George,’ said Ginny thoughtfully, ‘is that you sort of start thinking anything’s possible if you’ve got enough nerve.’” – Ginny Weasley, Harry Potter and the Order of the Phoenix, Chapter 29'],
+
+        some() {}
+    }
+}
+
+let message1 = create_message();
+console.log(message1.pets[0]);
