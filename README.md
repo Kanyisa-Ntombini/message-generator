8718332ac0ab070d31963f64a33e2f35a5aaacf2
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-02-22 19:45:17 +0200

    created index and css page. edited script file.

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 This is a Harry Potter themed message generator. A message will be randomly generated where a user will be told which Hogwarts house they belong to, the type of pet they have, their favourite subject and a quote from the books.
 
 ## Features
+I created a webpage that displays the messages. I've finished the structure of it using HTML. I am currently studying the CSS section. As I learn new concepts I will improve the styling.
+
+## How to use
+Please visit click this link: to view the webpage.
 
 ## Technologies used
 
--- script.js
+++ script.js
@@ -1,55 +1,54 @@
-function create_message() {
-    return {
-        _hogwarts_houses: ['Slytherin', 'Gryffindor', 'Ravenclaw', 'Hufflepuff'],
-        _pets: ['cat', 'owl', 'phoenix', 'hippogriff', 'dragon', 'toad'],
-        _subjects: ['Herbology', 'Defense Against The Dark Arts', 'Charms', 'Divination', 'Transfiguration', 'Astronomy', 'Potions', 'Flying Lessons'],
-        _quotes: ['"Do not pity the dead, Harry. Pity the living, and, above all those who live without love." – Albus Dumbledore', '"It is impossible to manufacture or imitate love." – Horace Slughorn', '"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends." – Albus Dumbledore', '"Books! And cleverness! There are more important things — friendship and bravery." – Hermione Granger', '"It does not do to dwell on dreams and forget to live." – Albus Dumbledore', '"Every human life is worth the same, and worth saving." — Kingsley Shacklebolt', '"If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals." – Sirius Black', '"When in doubt, go to the library." – Ron Weasley', '"We must unite inside [Hogwarts] / Or we’ll crumble from within / I have told you, I have warned you… / Let the Sorting now begin." — The Sorting Hat', '"Anything’s possible if you’ve got enough nerve." — Ginny Weasley', '"CONSTANT VIGILANCE!" — Barty Crouch, Jr'],
-
-        //getters
-        get hogwarts_houses() {
-            return this._hogwarts_houses;
-        },
-        get pets() {
-            return this._pets;
-        },
-        get subjects() {
-            return this._subjects;
-        },
-        get quotes() {
-            return this._quotes;
-        },
-
-        createRandomNum(arr) {
-            return Math.floor(Math.random() * arr.length);
-        },
-
-        message() {
-            let houseNum = this.createRandomNum(this.hogwarts_houses);
-            let petNum = this.createRandomNum(this.pets);
-            let subjectNum = this.createRandomNum(this.subjects);
-            let quoteNum = this.createRandomNum(this.quotes);
-
-            let house = this.hogwarts_houses[houseNum];
-            let pet = this.pets[petNum];
-            let subject = this.subjects[subjectNum];
-            let quote = this.quotes[quoteNum];
-
-            console.log(`Congratulations! You are in ${house} house.`);
-
-            if (pet[0] === 'a' || pet[0] === 'e' || pet[0] === 'i' || pet[0] === 'o' || pet[0] === 'u') {
-                console.log(`Your pet is an ${pet}.`);
-            } else {
-                console.log(`Your pet is a ${pet}`);
-            }
-            
-            console.log(`Your favourite subject is ${subject}.`);
-            console.log('A quote to live by:')
-            console.log(quote);
-
-            return '';
-        }
-    }
+function create_element_message(id, message) {
+	const newElement = document.createElement('span');
+	const newContent = document.createTextNode(message);
+	newElement.appendChild(newContent);
+	newElement.classList.add('message');
+	const referenceElement = document.getElementById(id);
+	referenceElement.append(newElement);
 }
 
-let message1 = create_message();
-console.log(message1.message());
+function display_message() {
+	const {house, pet, subject, quote} = create_message();
+	let removeElement = document.querySelectorAll('.message');
+	console.log(removeElement.length);
+
+	if (removeElement.length !== 0) {
+		for (let i = 0; i < removeElement.length; i++) {
+			removeElement[i].remove();
+		}
+	}
+
+  create_element_message('house', house);
+	create_element_message('pet', pet);
+	create_element_message('subject', subject);
+	create_element_message('quote', quote);
+}
+
+function createRandomNum(arr) {
+	return Math.floor(Math.random() * arr.length);
+}
+
+function create_message() {
+	const	hogwarts_houses = ['Slytherin', 'Gryffindor', 'Ravenclaw', 'Hufflepuff'];
+  const pets = ['Cat', 'Owl', 'Phoenix', 'Hippogriff', 'Dragon', 'Toad'];
+  const subjects = ['Herbology', 'Defense Against The Dark Arts', 'Charms', 'Divination', 'Transfiguration', 'Astronomy', 'Potions', 'Flying Lessons'];
+  const quotes = ['"Do not pity the dead, Harry. Pity the living, and, above all those who live without love." – Albus Dumbledore', '"It is impossible to manufacture or imitate love." – Horace Slughorn', '"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends." – Albus Dumbledore', '"Books! And cleverness! There are more important things — friendship and bravery." – Hermione Granger', '"It does not do to dwell on dreams and forget to live." – Albus Dumbledore', '"Every human life is worth the same, and worth saving." — Kingsley Shacklebolt', '"If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals." – Sirius Black', '"When in doubt, go to the library." – Ron Weasley', '"We must unite inside [Hogwarts] / Or we’ll crumble from within / I have told you, I have warned you… / Let the Sorting now begin." — The Sorting Hat', '"Anything’s possible if you’ve got enough nerve." — Ginny Weasley', '"CONSTANT VIGILANCE!" — Barty Crouch, Jr'];
+
+	const houseNum = createRandomNum(hogwarts_houses);
+	const petNum = createRandomNum(pets);
+	const subjectNum = createRandomNum(subjects);
+	const quoteNum = createRandomNum(quotes);
+
+	const house = hogwarts_houses[houseNum];
+	const pet = pets[petNum];
+	const subject = subjects[subjectNum];
+	const quote = quotes[quoteNum];
+
+	return {
+		house,
+		pet,
+		subject,
+		quote
+	}
+        
+}
